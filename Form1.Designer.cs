f6919f5ff700496ef3c7e9c77821e1053787e781
Haitao <wuhaitao99.st@gmail.com>
2021-09-21 15:27:27 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class txtSplit
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 70);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(200, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "分割";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(30, 150);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(525, 10);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(480, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 25);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(280, 60);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Text = "第.*章";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.Location = new System.Drawing.Point(30, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(420, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "选择文件 ->";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // txtSplit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 178);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "txtSplit";
+            this.ShowIcon = false;
+            this.Text = "txtSplit";
+            this.Load += new System.EventHandler(this.txtSplit_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class txtSplit : Form
+    {
+        private string newFilePath;
+        private StreamReader file;
+        private Regex regex;
+        private int fileLength;
+        public txtSplit()
+        {
+            InitializeComponent();
+        }
+
+        private void txtSplit_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "")
+            {
+                MessageBox.Show("请添加正则表达式");
+                return;
+            }
+            button2.Enabled = false;
+            button1.Enabled = false;
+            progressBar1.Value = 0;
+            label1.Text = "切割中";
+            regex = new Regex(textBox1.Text);
+            string line;
+            string text = "";
+            string newFileName;
+            int counter = 0;
+            int lineCounter = 0;
+            try
+            {
+                if (!Directory.Exists(newFilePath)) Directory.CreateDirectory(newFilePath);
+                while ((line = file.ReadLine()) != null)
+                {
+                    lineCounter++;
+                    progressBar1.Value = lineCounter * 100 / fileLength;
+                    if (regex.IsMatch(line))
+                    {
+                        Console.WriteLine(line);
+                        if (counter == 0)
+                        {
+                            text = $"{text}\n{line.Trim()}";
+                            counter++;
+                            continue;
+                        }
+                        newFileName = Path.Combine(newFilePath, $"{counter}.txt");
+                        File.WriteAllText(newFileName, text);
+                        text = "";
+                        counter++;
+                    }
+                    text = $"{text}\n{line.Trim()}";
+                }
+                newFileName = Path.Combine(newFilePath, $"{counter}.txt");
+                File.WriteAllText(newFileName, text);
+                file.Close();
+                label1.Text = $"切割完毕 - 共生成{counter}个文件";
+                progressBar1.Value = 100;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"出错了！{ex.Message}");
+                label1.Text = "切割失败";
+            }
+            button2.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "文本文件(*.txt)|*.txt";
+            openFileDialog.Title = "请选择文件";
+            openFileDialog.Multiselect = false;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                file = new StreamReader(openFileDialog.OpenFile());
+                button1.Enabled = true;
+                newFilePath = Path.Combine(Path.GetDirectoryName(openFileDialog.FileName), "out");
+                label1.Text = openFileDialog.FileName;
+                fileLength = File.ReadLines(openFileDialog.FileName).Count();
+            }
+        }
+    }
+}
